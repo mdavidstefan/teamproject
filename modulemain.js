835be16b73447372e91b805ff2e41af5468490c6
mdavidstefan <stefan.david.marton@gmail.com>
2024-04-26 08:36:57 +0200

    searching

--- modulemain.js
+++ modulemain.js
@@ -1,74 +1,70 @@
-let airline = []
+let city = [];
 
-let page = 1
-let query = "&query="
-let qStr = "San Francisco"
+let page = 1;
+let query = "&query=";
+let qStr = "";
 
 let options = {
-    method: 'GET',
-    headers: { 'x-api-key': 'FZWxIBwdLLHfGiwqyxVlmay17429U7C8icoXDAf9' }
-}
-const url = `https://api.api-ninjas.com/v1/city?name=San Francisco`
-const picurl = `https://api.unsplash.com/search/photos?client_id=Ajtp2bCaCXnMcwLDiOGZWtHh1vuCDM6bszAihbOe6g0&page=`
+  method: "GET",
+  headers: { "x-api-key": "FZWxIBwdLLHfGiwqyxVlmay17429U7C8icoXDAf9" },
+};
+const url = `https://api.api-ninjas.com/v1/city?name=${qStr}`;
+const picurl = `https://api.unsplash.com/search/photos?client_id=Ajtp2bCaCXnMcwLDiOGZWtHh1vuCDM6bszAihbOe6g0&page=`;
 //document.querySelector(".myBtn").addEventListener("click", renderCity)
 
-
-document.querySelector('#prevpage').addEventListener("click", prevPage)
-document.querySelector('#nextpage').addEventListener("click", nextPage)
+document.querySelector("#prevpage").addEventListener("click", prevPage);
+document.querySelector("#nextpage").addEventListener("click", nextPage);
+document.querySelector(".myBtn").addEventListener("click", searchCity);
 
 function prevPage() {
-    document.querySelector(".city-list").innerHTML = ""
-    page = page - 2
-    getData(picurl + page + query + qStr, renderCity)
+  document.querySelector(".city-list").innerHTML = "";
+  page = page - 2;
+  getData(picurl + page + query + qStr, renderCity);
 }
 
 function nextPage() {
-    document.querySelector(".city-list").innerHTML = ""
-    getData(picurl + page + query + qStr, renderCity)
+  document.querySelector(".city-list").innerHTML = "";
+  getData(picurl + page + query + qStr, renderCity);
 }
 
 fetch(url, options)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        airline = data;
-        showPictures()
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    city = data;
+    showPictures();
+  })
+  .catch((err) => {
+    console.log(`error ${err}`);
+  });
 
-    })
-    .catch(err => {
-        console.log(`error ${err}`);
-    })
-
-console.log(airline);
+console.log(city);
 
 const getData = async (url, renderFC) => {
-    const response = await fetch(url)
-    const data = await response.json()
-    renderFC(data)
-}
+  const response = await fetch(url);
+  const data = await response.json();
+  renderFC(data);
+};
 
 function showPictures(data) {
-    airline.forEach(item => {
-        document.querySelector('.city-list').innerHTML += `
-        <div class=" card"><img class="img-fluid" src="${item.logo_url}" alt="image of San Francisco"></div>
-        `
-    })
-    getData(picurl + page + query + qStr, renderCity)
-
+  getData(picurl + page + query + qStr, renderCity);
 }
 
 function renderCity(data) {
-    data.results.forEach(obj => {
-        const imageElement = document.createElement("img")
-        imageElement.src = obj.urls.small
-        imageElement.alt = obj.alt_description
-        document.querySelector(".city-list").appendChild(imageElement)
-    });
+  document.querySelector(".city-list").innerHTML = "";
+  data.results.forEach((obj) => {
+    const imageElement = document.createElement("img");
+    imageElement.src = obj.urls.small;
+    imageElement.alt = obj.alt_description;
+    document.querySelector(".city-list").appendChild(imageElement);
+  });   
 
-    console.log(page);
-    page++
+  console.log(page);
+  page++;
 }
 
-
-
-
+function searchCity(data) {
+  qStr = document.querySelector(".myInput").value;
+  console.log(qStr);
+  showPictures();
+}
